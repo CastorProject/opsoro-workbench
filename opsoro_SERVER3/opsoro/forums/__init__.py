8893a51eedb65237278f6605d3e581dffa6cc087
A. Darriba Frederiks <aduen.darribafrederiks@ugent.be>
2019-04-24 18:24:50 +0200

    removed full directory for submodule

--- opsoro_SERVER3/opsoro/forums/__init__.py
+++
@@ -1,4 +0,0 @@
-# import pkg_resources
-#
-# __version__ = pkg_resources.get_distribution("forums").version
-# default_app_config = "forums.apps.AppConfig"
--- opsoro_SERVER3/opsoro/opsoro/templatetags/social_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.tag  # 12/04/2019 possible incompatible update fix
-def user_can_disconnect(user_social_auth):
-    return user_social_auth.allowed_to_disconnect(user_social_auth.user, user_social_auth.provider)
